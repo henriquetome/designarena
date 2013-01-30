42cbb82fed5a1eeb6af0fe488fd74a65892964c2
henriquetome <henrique@e-soluttions.com,br>
2013-01-30 20:30:45 -0200

    Add more pages

--- js/functions.js
+++ js/functions.js
@@ -51,3 +51,34 @@ $(document).ready(function(){
         afterScroll: function() {}
     });
 });
+
+$(function() {
+	   if(!$.support.placeholder) {
+			   var active = document.activeElement;
+			   $(':text').focus(function () {
+					   if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+							   $(this).val('').removeClass('hasPlaceholder');
+					   }
+			   }).blur(function () {
+					   if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+							   $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+					   }
+			   });
+			   $(':text').blur();
+				$('textarea').focus(function () {
+					   if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+							   $(this).val('').removeClass('hasPlaceholder');
+					   }
+			   }).blur(function () {
+					   if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+							   $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+					   }
+			   });
+			   $('textarea').blur();
+
+			   $(active).focus();
+			   $('form').submit(function () {
+					   $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+			   });
+	   }
+});
