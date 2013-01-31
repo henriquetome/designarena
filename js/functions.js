e689f258e05f597bf2abd8ba4de9b9c40d8f2389
henriquetome <henrique@e-soluttions.com.br>
2013-01-31 20:48:48 -0200

    Add button portfolio

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+//FLOATING MENU
 var MENU = {
 	sectionsHeight: {},
 
@@ -12,7 +13,6 @@ var MENU = {
 				var scrollTop = $(window).scrollTop();
 				var windowHeight = $(window).height();
 				var menuBox = $("#drop-nav");
-
 				var homeHeight = $("#home").height();
 				var portHeight = $("#portfolio").height() + homeHeight;
 				var clienttHeight = $("#clientes").height() + portHeight;
@@ -32,26 +32,24 @@ var MENU = {
 				}
 
 				if (scrollTop > 200 && parseInt(menuBox.css("margin-top")) != "13") {
-
 					menuBox.css("margin-top", "13px");
-
 				} else if (scrollTop > 100 && scrollTop < 200) {
-
 					var perc = 1 - ((scrollTop - 100) / 100);
 					menuBox.css('margin-top', parseInt((13 - (93 * perc))));
-
 				}
 			});
 		});
 	}
 };
 
+//ROLL PAGE MENU
 $(document).ready(function(){
     $('a.main-btn').smoothScroll({
         afterScroll: function() {}
     });
 });
 
+//PLACEHOLDER
 $(function() {
 	   if(!$.support.placeholder) {
 			   var active = document.activeElement;
@@ -81,4 +79,13 @@ $(function() {
 					   $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
 			   });
 	   }
+});
+
+//SLIDE PORTFÓLIO
+$(function(){
+	$("ul#works li").hover(function(){
+		$('img', this).animate({top: "213px"}, {queue:false, duration:400});
+	}, function(){
+		$('img', this).animate({top: "0"}, {queue:false, duration:400})
+	});
 });
